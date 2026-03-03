b0b962c517ccbf1583fe4fe2c40017dd25d62e05
ericoborgonove <borgonove@gmail.com>
2026-03-02 21:05:00 -0400

    feat: aula de rotas

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = 3002;
 
-app.get ('/saude', (req, res)=>{
-    
+app.get ('/algo', (req,res)=> {
+    res.sendFile(path.resolve('algo.html'))
 });
 
-
+app.get ('/saude', (req, res)=>{
+    res.status(200).send('Aplicação Saudável')
+});
+app.get ('/jack', (req, res)=>{
+    res.status(500).send('MacDonalds nada Saudável')
+});
 
 // sempre o ultimo
 app.listen(PORT, () => {
