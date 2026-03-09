853d6de46c4544b39cbd7b33ea34942ca2605c83
ericoborgonove <borgonove@gmail.com>
2026-03-09 19:57:03 -0400

    dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require ('dotenv').config();
 const express = require('express');
 const app = express();
 const general = require('./routes/general.routes');
-const PORT = 3000;
+//const PORT = 3000;
 
 app.use('/general', general);
 
@@ -10,6 +11,6 @@ app.use('/general', general);
 app.use ((req, res, next) => {
     res.status(404).send("Não Encontrado")
 });
-app.listen(PORT, () => {
-    console.log (`App rodando na porta ${PORT}`)
+app.listen(process.env.PORT, () => {
+    console.log (`App rodando na porta ${process.env.PORT}`)
 });
