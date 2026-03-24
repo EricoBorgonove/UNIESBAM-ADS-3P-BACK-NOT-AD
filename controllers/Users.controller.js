f070d7083c994940089e566274ba69cd9649de32
ericoborgonove <borgonove@gmail.com>
2026-03-23 20:47:08 -0400

    controller users, createuser

---
+++ controllers/Users.controller.js
@@ -0,0 +1,25 @@
+const { Users } = require ('../models');
+const { Op } = require('sequelize');
+
+module.exports = {
+    // create - criar um novo usuário
+
+    async createUser (req, res){
+        try{
+            const { nome, cpf, email, senha, tipo_user } = req.body;
+            if (! ['admin', 'user', 'dev'].includes(tipo_user)){
+                return res.status(400).json({message: 'Tipo de usuário inválido'});
+            }
+            const userExistente = await Users.findOne({where: { email }});
+            if (userExistente){
+                return res.status(400).json({message: 'Email já cadastrado !'});
+            }
+            // await Users.create ({nome, cpf, email, senha, tipo_user});
+            const user = await Users.create ({nome, cpf, email, senha, tipo_user});
+            // return res.status(201)
+            return res.status(201).json(user)
+        }catch (error){
+            return res.status(500).json({message: 'Erro ao criar usuário'});
+        }
+    }
+}
