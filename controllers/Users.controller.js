e319c5afbeb101b91141fa0db71bb83d1c93c59e
ericoborgonove <borgonove@gmail.com>
2026-03-30 21:15:26 -0400

    controller de usuários

--- controllers/Users.controller.js
+++ controllers/Users.controller.js
@@ -21,5 +21,70 @@ module.exports = {
         }catch (error){
             return res.status(500).json({message: 'Erro ao criar usuário'});
         }
+    },
+
+    // READ - Listar todos os Usuários
+    async getAllUsers (req, res){
+        try {
+            const users = await Users.findAll({
+                attributes: {exclude: ['senha']}
+            });
+            return res.json(users);
+        } catch (error) {
+            return res.status(500).json({message: 'Erro ao listar usuários', error: error.message});
+        }
+    },
+
+    // READ - Buscar usuário por ID
+
+    async getUserById (req, res){
+        try {
+            const { id } = req.params;
+            const user = await Users.findByPk(id,{
+                attributes: {exclude: ['senha']}
+            });
+            if (!user) return res.status(404).json({message: 'Usuário não Encontrado !'});
+            return res.json(user);
+
+        } catch (error) {
+            return res.status(500).json({message: 'Erro ao buscar usuário', error: error.message});
+        }
+    },
+    // UPDATE - Atualizar usuário
+
+    async updateUser (req, res){
+        try {
+            const { id } = req.params;
+            const { nome, cpf, email, senha, tipo_user } = req.body;
+
+            if (! ['admin', 'user', 'dev'].includes(tipo_user)){
+                return res.status(400).json({message: 'Tipo de usuário inválido'});
+            }
+            const user = await Users.findByPk(id,{
+                attributes: {exclude: ['senha']}
+            });
+            if (!user) return res.status(404).json({message: 'Usuário não Encontrado !'});
+            
+            await user.update ({nome, cpf, email, senha, tipo_user});
+            return res.status(201);
+        } catch (error) {
+            return res.status(500).json({message: 'Erro ao atualizar o usuário', error: error.message});
+        }
+    },
+    // DELETE - Remover Usuário
+
+    async deleteUser (req, res){
+        try {
+            const { id } = req.params;
+            const user = await Users.findByPk(id,{
+                attributes: {exclude: ['senha']}
+            });
+            if (!user) return res.status(404).json({message: 'Usuário não Encontrado !'});
+            await user.destroy();
+            return res.status(204);
+            
+        } catch (error) {
+            return res.status(500).json({message: 'Erro ao excluir o usuário', error: error.message});
+        }
     }
 }
